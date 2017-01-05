8162e93e38d91b9bcc4f6e8e4daa6d16903ed7f5
socialcapsule <socialcapsule899@gmail.com>
2017-01-05 15:12:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'hHYLeAzeslXXfyi5eRuk',
+  masterKey: process.env.MASTER_KEY || 'UeUiyLviH8Ejb4lsLWz1', //Add your master key here. Keep it secret!
+  clientKey: process.env.CLIENT_KEY || '8EV6rAgdJBwJcxeq3ULl', // Add your client key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
