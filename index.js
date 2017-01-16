8bd0a22e5a2593d97502c390a6bacc3cb05e9385
socialcapsule <socialcapsule899@gmail.com>
2017-01-16 19:09:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,9 +40,7 @@ var api = new ParseServer({
  passwordResetBodyHTML: "<!--DOCTYPE html>........"
  }
  }
-  
-  
-});
+ });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
