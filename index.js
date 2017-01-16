ffd31b7583f6391a7870483e69431108daa03114
socialcapsule <socialcapsule899@gmail.com>
2017-01-16 16:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  
+  verifyUserEmails: true,
+publicServerURL: ' https://capsulesocial.herokuapp.com/parse',
+appName: 'socialcapsule',
+emailAdapter: { 
+    module: 'parse-server-simple-mailgun-adapter',
+    options: { 
+               fromAddress: 'parse@example.com',
+               domain: 'betabulls.com', 
+               apiKey: 'key-b3a8487041c3ea3bcbc6a57031504773', 
+             }
+ },
+  
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
